de5f27584af5df78479e2b9af438e35b65298364
Larry Garfield <garfield@palantir.net>
2013-07-18 15:38:16 -0500

    Add licensing information.

--- src/PalantirNet/RottenTomatoes/MovieNotFoundException.php
+++ src/PalantirNet/RottenTomatoes/MovieNotFoundException.php
@@ -1,8 +1,12 @@
 <?php
 
-/**
- * @file
- * Contains \PalantirNet\RottenTomatoes\MovieNotFoundException.
+/*
+ * This file is part of the Rotten Tomatoes package.
+ *
+ * (c) Palantir.net http://www.palantir.net/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace PalantirNet\RottenTomatoes;
@@ -11,5 +15,5 @@ namespace PalantirNet\RottenTomatoes;
  * Thrown when a movie is retrieved from Rotten Tomatoes that does not exist.
  */
 class MovieNotFoundException extends \RuntimeException {
-  
+
 }
--- src/PalantirNet/RottenTomatoes/ObjectBase.php
+++ src/PalantirNet/RottenTomatoes/ObjectBase.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Rotten Tomatoes package.
+ *
+ * (c) Palantir.net http://www.palantir.net/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace PalantirNet\RottenTomatoes;
 
 /**
